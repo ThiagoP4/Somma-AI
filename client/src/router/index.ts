34cbb843bf1d15bbf6ec705bfdeced71399382f5
ThiagoP4 <thiagodminas0@gmail.com>
2026-02-08 17:22:38 -0300

    'from' is declared but its value is never read.

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -30,7 +30,7 @@ const router = createRouter({
     ]
 });
 
-router.beforeEach(async (to, from, next) => {
+router.beforeEach(async (to, next) => {
     const { data: { session } } = await supabase.auth.getSession();
 
     if (to.meta.requiresAuth && !session) {
